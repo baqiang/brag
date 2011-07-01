3c89eecdd30adf1b06a605a3119ce12168c3656d
baqiang <baq.james@gmail.com>
2011-07-01 13:26:06 +0800

    dynamic pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -15,5 +15,11 @@ describe PagesController do
       response.should be_success
     end
   end
-
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
 end
