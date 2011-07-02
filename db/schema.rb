64dcb7d469962c246beeee1a33d169d9b4769623
baqiang <baq.james@gmail.com>
2011-07-02 14:28:59 +0800

    sign in setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110702020200) do
+ActiveRecord::Schema.define(:version => 20110702062329) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -18,5 +18,7 @@ ActiveRecord::Schema.define(:version => 20110702020200) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
