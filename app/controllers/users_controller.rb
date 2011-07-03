483f5819a34c2de6a4c62738e3e10698e6b61202
baqiang <baq.james@gmail.com>
2011-07-03 10:41:57 +0800

    finish sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,18 @@ class UsersController < ApplicationController
   end
   
   def show
-      @user = User.find(params[:id])
-      @title = @user.name
+     @user = User.find(params[:id])
+     @title = @user.name
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to @user, :flash => { :success => "Welcom to iBragu!" }
+    else
+      @title = "Sign up"
+      render 'new'
+    end
   end
 end
 
