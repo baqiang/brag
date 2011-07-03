cf25bbc44fd47b3786a9a82447268199a8e55086
baqiang <baq.james@gmail.com>
2011-07-03 11:45:15 +0800

    add sign

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Brag::Application.routes.draw do
   
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   root :to => "pages#home"
   
@@ -11,6 +12,10 @@ Brag::Application.routes.draw do
   match '/help', :to => 'pages#help'
   
   match '/signup', :to => 'users#new'
+  
+  match '/signin', :to => 'sessions#new'
+  
+  match '/signout', :to => 'seesions#destroy'
   
   
   # The priority is based upon order of creation:
